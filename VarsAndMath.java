a20d1d3fa881bb331c7ff4e682fe06cc645ee380
Brian Finley <bkfinley1@my.scccd.edu>
2024-01-25 17:59:12 -0800

    Replacing VarsAndMath.java with mine

--- VarsAndMath.java
+++ VarsAndMath.java
@@ -1,38 +1,18 @@
 public class VarsAndMath {
-    public static void main(String[] args) {
-
+    public static void main (String[] args) {
         System.out.println("\n Welcome to variables and math !! \n");
+    
 
-        // A variable is a storage location in memory with a specific type and a name.
-        /* this is a longer comment
+    // Declaring an integer named 'a' and giving it a value of 10
+    int a = 10;
 
-          comments are ignored but the compiler, but are important for the programmer!
-         */
+    // Declaring a double named 'b' and giving is a value of 5.5
+    double b = 5.5;
 
-       // Declaring an integer variable named 'a' and initializing it with the value 10
-        int a = 10;
+    // Performing the math on 'a' and 'b' and storing the answer in a double called 'sum'
+    double sum = a + b;
 
-        // Declaring a double variable named 'b' and initializing it with the value 5.5
-        double b = 5.5;
-
-        // Performing a mathematical operation: adding 'a' and 'b'
-        double sum = a + b;
-
-        System.out.println("\n\n The sum of " + a + " and " + b + " is " + sum);
-
-        // Variables can also be used to store the results of other operations
-        double product = a * b;
-        System.out.println("The product of " + a + " and " + b + " is: " + product);
-
-        // Demonstrating variable value change
-        a = 20; // Changing the value of 'a' to 20
-        sum = a + b; // Recalculating sum with the new value of 'a'
-        System.out.println("\n\nThe new sum of " + a + " and " + b + " is: " + sum + "\n\n");
-
-
-
-
-
-
+    // Outputting the sum to the screen
+    System.out.println("\n\n The sum of " + a + " and " + b + " is " + sum);
     }
 }
