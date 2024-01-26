131bc14328ff4af11ce1a7c3a39529e2f615abb5
Brian Finley <bkfinley1@my.scccd.edu>
2024-01-25 20:14:09 -0800

    Created Methods.java

---
+++ Module-1/Methods.java
@@ -0,0 +1,11 @@
+public class Methods {
+    public static void main(String[] args) {
+       String name = myName("Brian");
+       System.out.println("\n\n" + name + "\n\n");
+    }
+
+    private static String myName (String name) {
+        return "My name is " + name;
+    }
+    
+}
